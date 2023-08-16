68b27f095cdc923d02e5d085c853bd8e60ecc7e2
Jordan Defay <140656904+jordandefay@users.noreply.github.com>
2023-08-16 20:42:14 +0100

    Create priseDeNotes.c

---
+++ prisesDeNotes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+// Pour compiler sous linux : gcc <nom_du_fichier_entree> -o <nom_du_fichier_sortie>
+/*
+On peut écrire
+des commentaires
+sur plusieurs lignes
+de cette maniére
+*/
+
+// Pour afficher un print sur la console
+int main(void)
+{
+	printf("Bjr tout le monde !\n");
+	return 0;
+}
+
+// LES VARIABLES
+/* Les mots réservés sous C :
+
+auto ; enum ; return ; unsigned
+break ; extern ; short ; void
+case ; float ; signed ; volatile
+char ; for ; sizeof ; while
+const ; goto ; static
+default ; if ; struct
+do ; int ; switch
+double ; long ; typedef
+else ; register ; union
+*/
+int main(void)
+{
+	//Déclaration d'une variable de type entier (int)
+	int aireDuCarre = 0; //aireDuCarre sera stocké (par exemple dans 0xF500DA)
+
+	float prixReservation = 15.345;
+	signed char = 120;
+	signed char = 'A';
+	return 0;
+}
